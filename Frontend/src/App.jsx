e6875b7d3304a36f4b1ee9576999cfb7a28f6e85
Geetanjali310 <anjalinegi5759@gmail.com>
2024-12-08 20:09:34 +0530

    Project completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
+import React from 'react';
+import Home from './home/Home';
+import Course from "./components/Course";
+import { Navigate, Route ,Routes} from 'react-router-dom';
+import Courses from './courses/Courses';
+import Signup from './components/Signup';
+import {Toaster} from "react-hot-toast";
+import {useAuth} from "./context/AuthProvider"
  
-const App = () => {
+function App()  {
+  const[authUser,setAuthUser]=useAuth()
+  console.log(authUser)
   return (
-    <div>
-     <Navbar />
-     <Banner />
-     <Footer />
-    </div>
-  )
+    <>
+  
+   <div className='dark:bg-slate-900 dark:text-white'>
+   <Routes>
+   <Route path="/" element={<Home/>}/>
+   <Route path="/course" element={authUser?<Courses/>:<Navigate to="/signup"/>}/>
+   <Route path="/signup" element={<Signup/>}/>
+
+   </Routes>
+   <Toaster/>
+   </div>
+    
+    </>
+  );
 }
 
 export default App
